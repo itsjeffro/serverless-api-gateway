112ebf82140b03f92245319d88d3e7a954e1a012
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-21 23:28:41 +1100

    Added example

--- handler.js
+++ handler.js
@@ -1,3 +1,16 @@
 module.exports.clientAuthorizer = async (event, context, callback) => {
-  //
-}
+  // @TODO Replaces example
+  context.succeed({
+    "principalId": 'userId',
+    "policyDocument": {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "execute-api:Invoke",
+          "Effect": 'allow',
+          "Resource": event.methodArn
+        }
+      ]
+    }
+  });
+};
