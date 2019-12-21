ac0f385491914ce7dec73704b22b790c2b974142
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-22 08:11:15 +1100

    Added jwt authorizer logic

--- handler.js
+++ handler.js
@@ -1,14 +1,10 @@
 let jwt = require('jsonwebtoken');
 
-module.exports.authorizer = async (event, context, callback) => {
+module.exports.clientAuthorizer = async (event, context, callback) => {
   try {
     let token = event.authorizationToken;
     let decodedToken = jwt.verify(token, process.env.JWT_SIGNING_KEY);
     let effect = 'Allow';
-
-    if (!decodedToken.hasOwnProperty('scopes')) {
-      return callback('Unauthorized');
-    }
     
     return callback(null, generatePolicy(effect, event, decodedToken));
   } catch (e) {
