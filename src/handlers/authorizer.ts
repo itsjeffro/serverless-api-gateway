5d1c6867356268c6e45db43c5a088611e459eaae
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-03-14 22:45:19 +1100

    Added logging

--- src/handlers/authorizer.ts
+++ src/handlers/authorizer.ts
@@ -7,14 +7,24 @@ import DecodedTokenInterface from '../lib/Jwt/DecodedTokenInterface';
 module.exports.auth = async (event: LambdaEventInterface, context: any, callback: any) => {
   try {
     // Retrieve token from lambda event.
+    console.log("Getting token...");
+
     const auth = new Auth(event);
     const token = auth.getToken();
 
+    console.log("Retrieved token.");
+
     // Verify token and returns decoded token.
+    console.log("Verifying token...");
+
     const signingKey = process.env.JWT_SIGNING_KEY || "";
     const verifiedToken = jwt.verify(token, signingKey);
+
+    console.log("Verified token.");
     
     // Generate policy document.
+    console.log("Generating policy document...");
+
     const decodedToken = verifiedToken as DecodedTokenInterface;
     const policy = new Policy(event);
 
@@ -25,6 +35,8 @@ module.exports.auth = async (event: LambdaEventInterface, context: any, callback
       user: decodedToken.sub,
       company: decodedToken.company
     });
+
+    console.log(`Generated policy document: ${JSON.stringify(policyDocument)}`);
     
     return callback(null, policyDocument);
   } catch (e) {
