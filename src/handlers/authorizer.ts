65480ba6b3fa90b37d43f47f860d9e1e72e19ff5
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-03-15 17:20:37 +1100

    Add allowed resources retrieve from policy table

--- src/handlers/authorizer.ts
+++ src/handlers/authorizer.ts
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
+import AWS from "aws-sdk";
+
 import LambdaEventInterface from "../lib/LambdaEvent/LambdaEventInterface";
-import Auth from "../lib/Auth";
-import PolicyDocument from "../lib/Auth/PolicyDocument";
+import LambdaEvent from "../lib/LambdaEvent";
+import PolicyDocument from "../lib/PolicyDocument";
 import DecodedTokenInterface from '../lib/Jwt/DecodedTokenInterface';
+import ServicePolicy from '../lib/ServicePolicy/ServicePolicy';
+
+let dynamoDb = new AWS.DynamoDB({ region: "ap-southeast-2" });
 
 module.exports.auth = async (event: LambdaEventInterface, context: any, callback: any) => {
   try {
     // Retrieve token from lambda event.
     console.log("Getting token...");
 
-    const auth = new Auth(event);
-    const token = auth.getToken();
+    const lambdaEvent = new LambdaEvent(event);
+    const token = lambdaEvent.getToken();
 
     console.log("Retrieved token.");
 
@@ -21,14 +26,27 @@ module.exports.auth = async (event: LambdaEventInterface, context: any, callback
     const verifiedToken = jwt.verify(token, signingKey);
 
     console.log("Verified token.");
+
+    // Retrieving service policy from dynamoDB.
+    console.log("Retrieving policy...");
+
+    const servicePolicy = new ServicePolicy(dynamoDb, "internal_service_policies");
+    const servicePolicyMethods: any = await servicePolicy.getPolicyByServiceNameVersion("posts-v1");
     
     // Generate policy document.
     console.log("Generating policy document...");
 
     const decodedToken = verifiedToken as DecodedTokenInterface;
-    const policy = new PolicyDocument(event);
+    const policy = new PolicyDocument(event, decodedToken.sub);
 
-    policy.addAllowedResource("*");
+    for (let i = 0; i < servicePolicyMethods.length; i++) {
+      let methodArn = lambdaEvent.getMethodArn({ 
+        method: servicePolicyMethods[i].method,
+        resource: servicePolicyMethods[i].resource
+      });
+
+      policy.addAllowedResource(methodArn);
+    }
 
     const policyDocument = policy.generate('Allow', {
       issuer: decodedToken.iss,
