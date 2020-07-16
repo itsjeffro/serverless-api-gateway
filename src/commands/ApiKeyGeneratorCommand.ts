fc04e4afbcb0ae4e1f581802ff444a849bb1db96
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-07-16 21:41:32 +1000

    Added initial api generation

---
+++ src/commands/ApiKeyGeneratorCommand.ts
@@ -0,0 +1,18 @@
+import String from '../lib/String';
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const string = new String();
+const key = string.random(7);
+const randomString = string.random(80);
+
+const plainTextHash = `${key}.${randomString}`;
+
+bcrypt.hash(plainTextHash, saltRounds, async function(err: any, hash: any) {
+  console.log("plaintext: ", plainTextHash);
+
+  console.log("hashed: ", hash);
+
+  console.log("matches: ", await bcrypt.compareSync(plainTextHash, hash));
+});
