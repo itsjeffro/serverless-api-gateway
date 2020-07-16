981a27d4597a15805175b0117d15620af088fbed
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-07-16 21:59:42 +1000

    Added hashing config

--- src/commands/ApiKeyGeneratorCommand.ts
+++ src/commands/ApiKeyGeneratorCommand.ts
@@ -1,14 +1,17 @@
 import String from '../lib/String';
 
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const hashing = require('../../config/hashing');
 
 const string = new String();
 const key = string.random(7);
+
 const randomString = string.random(80);
 const plainTextHash = randomString;
 
-bcrypt.hash(plainTextHash, saltRounds, async function(err: any, hash: any) {
+const rounds = hashing.bcrypt.rounds;
+
+bcrypt.hash(plainTextHash, rounds, async function(err: any, hash: any) {
   console.log("plaintext: ", `${key}.${plainTextHash}`);
 
   console.log("hashed: ", hash);
