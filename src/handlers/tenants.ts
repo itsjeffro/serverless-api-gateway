7cbe7e8d9a92730032cca984020c24d26d6564fd
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-07-18 22:04:21 +1000

    Added tenant create export module

--- src/handlers/tenants.ts
+++ src/handlers/tenants.ts
@@ -1,5 +1,7 @@
 import AWS from "aws-sdk";
 import TenantRepository from "../repository/TenantRepository";
+import Tenant from "../services/Tenant";
+import LambdaEvent from "../lib/LambdaEvent";
 
 let dynamoDb = new AWS.DynamoDB({ region: "ap-southeast-2" });
 
@@ -11,5 +13,40 @@ module.exports.list = async (event: any) => {
   return {
     statusCode: 200,
     body: JSON.stringify(await tenantRepository.getAll()),
+  };
+}
+
+module.exports.create = async (event: any) => {
+  const lambdaEvent = new LambdaEvent(event);
+  const tenant = new Tenant(dynamoDb);
+
+  let eventBody = lambdaEvent.getBody();
+
+  let code = null;
+  let body = null;
+
+  const results = await tenant.createTenant({
+    tenant_name: eventBody.tenant_name || "",
+    full_name: eventBody.full_name || "",
+  });
+
+  if (null !== results) {
+    code = 201;
+    body = results;
+  } else if (null === results) {
+    code = 422;
+    body = {
+      message: "The following errors occurred",
+      errors: {
+        tenant_name: [
+          `Partition key [${ eventBody.tenant_name }] already exists`
+        ]
+      }
+    };
+  }
+
+  return {
+    statusCode: code,
+    body: JSON.stringify(body),
   };
 }
