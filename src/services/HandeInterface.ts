60552131ae8eb48175a248c46830b2b106d34ae7
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-07-19 14:05:22 +1000

    Moved files

--- src/services/HandeInterface.ts
+++
@@ -1,9 +0,0 @@
-import LambdaEventInterface from "src/lib/LambdaEvent/LambdaEventInterface";
-
-import LambdaEvent from '../lib/LambdaEvent';
-
-interface HandleInterface {
-  handle(lambdaEvent: LambdaEvent): any
-}
-
-export default HandleInterface;
