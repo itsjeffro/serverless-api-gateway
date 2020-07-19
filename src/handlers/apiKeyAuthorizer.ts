51cf3218bb933db2f682239cf81c073023cb01b1
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-07-19 20:46:55 +1000

    Moved logic to own service

--- src/handlers/apiKeyAuthorizer.ts
+++ src/handlers/apiKeyAuthorizer.ts
@@ -1,5 +1,4 @@
 import AWS from "aws-sdk";
-const bcrypt = require("bcryptjs");
 
 import LambdaEventInterface from "../lib/LambdaEvent/LambdaEventInterface";
 import LambdaEvent from "../lib/LambdaEvent";
@@ -7,73 +6,31 @@ import Logger from '../lib/Logger';
 import ApiKeyRepository from "../repository/ApiKeyRepository";
 import PolicyDocument from "../lib/PolicyDocument";
 import ServicePolicyRepository from "../repository/ServicePolicyRepository";
+import ApiKeyAuthorizer from "../services/Functions/ApiKeyAuthorizer";
 
 let dynamoDb = new AWS.DynamoDB({ region: "ap-southeast-2" });
 
 module.exports.auth = async (event: LambdaEventInterface, context: any, callback: any) => {
+  const stage = process.env.STAGE || "";
+
   const logger = new Logger(console);
   const lambdaEvent = new LambdaEvent(event);
   const policyDocument = new PolicyDocument(lambdaEvent);
-
-  const stage = process.env.STAGE || "";
-
-  const bearer = lambdaEvent.getToken();
-  const apiKeySegments = bearer.split('.');
-
-  if (apiKeySegments.length !== 2) {
-    logger.log(`Segments count is [${ apiKeySegments.length }] expected 2`);
-    
-    return callback("Unauthorized"); 
-  }
-
-  // Api key
   const apiKeyRepository = new ApiKeyRepository(dynamoDb, stage);
-  
-  let key = apiKeySegments[0];
-  let plainTextHash = apiKeySegments[1];
-  
-  let apiKey = await apiKeyRepository.getOneByIdentifierKey(key);
-  let hash = apiKey ? apiKey.hash : "";
-  let permissions = apiKey ? apiKey.permissions.split(",") : [];
-  let isVerified = await bcrypt.compareSync(plainTextHash, hash);
-  
-  // Service
-  const serviceRepository = new ServicePolicyRepository(dynamoDb, stage);
+  const servicePolicyRepository = new ServicePolicyRepository(dynamoDb, stage);
 
-  const methodArn = lambdaEvent.getMethodArnSegments();
-  const resource = methodArn.resource;
-  const resourceSegments = resource.split('/');
-  const versionCheck = /v[0-9]/g;
+  try {
+    const apiKeyAuthorizer = new ApiKeyAuthorizer(
+      logger,
+      apiKeyRepository,
+      servicePolicyRepository,
+      policyDocument
+    );
 
-  let serviceName = resourceSegments[0];
-
-  if (versionCheck.test(resourceSegments[0])) {
-    const version = resourceSegments[0];
-    const service = resourceSegments[1];
-
-    serviceName = `${service}-${version}`;
-  }
-  
-  let servicePolicy = await serviceRepository.getPolicyByServiceNameVersion(serviceName);
-
-  logger.log({
-    key: key,
-    isRecord: apiKey !== null,
-    isHashCorrect: isVerified,
-    permissions: permissions,
-  });
-
-  logger.log(servicePolicy);
-
-  if (isVerified) {
-    const document = policyDocument
-      .setPrincipalId(key)
-      .setAvailablePolicies(servicePolicy)
-      .setPermissions(permissions)
-      .generate();
+    let document = await apiKeyAuthorizer.handle(lambdaEvent);
 
     return callback(null, document);
+  } catch (e) {
+    return callback("Unauthorized");
   }
-
-  return callback("Unauthorized");
 }
--- src/services/Functions/ApiKeyAuthorizer/ApiKeyAuthorizer.ts
+++ src/services/Functions/ApiKeyAuthorizer/ApiKeyAuthorizer.ts
@@ -1,5 +1,136 @@
-class ApiKeyAuthorizer {
-  //
+const bcrypt = require("bcryptjs");
+
+import HandleInterface from "../HandeInterface";
+import LambdaEvent from "../../../lib/LambdaEvent";
+import LoggerInterface from "../../../lib/Logger/LoggerInterface";
+import ServicePolicyRepository from "../../../repository/ServicePolicyRepository";
+import ApiKeyRepository from "../../../repository/ApiKeyRepository";
+import PolicyDocument from "../../../lib/PolicyDocument";
+
+class ApiKeyAuthorizer implements HandleInterface {
+  logger: LoggerInterface;
+  apiKeyRepository: ApiKeyRepository;
+  servicePolicyRepository: ServicePolicyRepository;
+  policyDocument: PolicyDocument;
+
+  constructor(
+    logger: LoggerInterface,
+    apiKeyRepository: ApiKeyRepository,
+    servicePolicyRepository: ServicePolicyRepository,
+    policyDocument: PolicyDocument
+  ) {
+    this.logger = logger;
+    this.apiKeyRepository = apiKeyRepository;
+    this.servicePolicyRepository = servicePolicyRepository;
+    this.policyDocument = policyDocument;
+  }
+
+  async handle(lambdaEvent: LambdaEvent): Promise<any> {
+    const bearer = lambdaEvent.getToken();
+    const apiKeySegments = bearer.split('.');
+  
+    if (apiKeySegments.length !== 2) {
+      this.logger.log(`Segments count is [${ apiKeySegments.length }] expected 2`);
+      
+      throw new Error("Unauthorized");
+    }
+  
+    // Api key
+    let key = apiKeySegments[0];
+    let plainTextHash = apiKeySegments[1];
+    
+    let apiKey = await this.apiKeyRepository.getOneByIdentifierKey(key);
+    let hash = apiKey ? apiKey.hash : "";
+    let permissions = apiKey ? apiKey.permissions.split(",") : [];
+    let isVerified = await bcrypt.compareSync(plainTextHash, hash);
+
+    this.logger.log(permissions);
+
+    // Get services
+    let services = this.getServicesFromPermissions(permissions);
+
+    permissions = this.getPermissions(permissions);
+
+    this.logger.log(services);
+    
+    // Service
+    let policies = {};
+
+    for (let i = 0; i < services.length; i++) {
+      let serviceName = this.getServiceName(services[i]);
+
+      let servicePolicy = await this.servicePolicyRepository.getPolicyByServiceNameVersion(serviceName);
+
+      policies = {
+        ...policies,
+        ...servicePolicy
+      };
+    }
+
+    this.logger.log(policies);
+  
+    this.logger.log({
+      key: key,
+      isRecord: apiKey !== null,
+      isHashCorrect: isVerified,
+      permissions: permissions,
+    });
+  
+    if (isVerified) {
+      const document = this.policyDocument
+        .setPrincipalId(key)
+        .setAvailablePolicies(policies)
+        .setPermissions(permissions)
+        .generate();
+  
+      return document;
+    }
+  
+    throw new Error("Unauthorized");
+  }
+
+  getServicesFromPermissions(permissions: string[]): string[] {
+    let services = [];
+
+    for (let i = 0; i < permissions.length; i++) {
+      let permissionSegements = permissions[i].split(".");
+      let permission = permissionSegements[0];
+
+      if (services.indexOf(permission) === -1) {
+        services.push(permission);
+      }
+    }
+
+    return services;
+  }
+
+  getServiceName(resource: string): string {
+    const resourceSegments = resource.split('/');
+    const versionCheck = /v[0-9]/g;
+  
+    let serviceName = resourceSegments[0];
+  
+    if (versionCheck.test(resourceSegments[0])) {
+      const version = resourceSegments[0];
+      const service = resourceSegments[1];
+  
+      serviceName = `${service}-${version}`;
+    }
+
+    return serviceName;
+  }
+
+  getPermissions(permissions: string[]): string[] {
+    return permissions.map((permission: string) => {
+      let permissionSegments = permission.split("/");
+
+      if (permissionSegments.length === 2) {
+        return permissionSegments[1];
+      }
+
+      return permissionSegments[0];
+    });
+  }
 }
 
 export default ApiKeyAuthorizer;
